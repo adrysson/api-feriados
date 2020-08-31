4ea290a43d88c4db518b48df5a919b55146b427a
Adrysson <adrysson@doity.com.br>
2020-08-31 02:17:35 -0300

    Controller com es6

--- controllers/holiday.js
+++ controllers/holiday.js
@@ -1,88 +1,82 @@
-const holiday = require('../models').Holiday
+import { Holiday as holiday } from '../models'
 
-module.exports = {
-  async index(req, res) {
-    return holiday
-      .findAll({
-        order: [['created_at', 'DESC']],
-      })
-      .then((holidays) => {
-        res.status(200).send(holidays)
-      })
-      .catch((error) => {
-        res.status(400).send(error)
-      })
-  },
-
-  getById(req, res) {
-    return Holiday.findByPk(req.params.id, {
-      include: [
-        {
-          model: Student,
-          as: 'students',
-        },
-      ],
+export async function index(req, res) {
+  return holiday
+    .findAll({
+      order: [['created_at', 'DESC']],
     })
-      .then((holiday) => {
-        if (!holiday) {
-          return res.status(404).send({
-            message: 'Holiday Not Found',
-          })
-        }
-        return res.status(200).send(holiday)
-      })
-      .catch((error) => {
-        console.log(error)
-        res.status(400).send(error)
-      })
-  },
-
-  add(req, res) {
-    return Holiday.create({
-      class_name: req.body.class_name,
+    .then((holidays) => {
+      res.status(200).send(holidays)
     })
-      .then((holiday) => res.status(201).send(holiday))
-      .catch((error) => res.status(400).send(error))
-  },
-
-  update(req, res) {
-    return Holiday.findByPk(req.params.id, {
-      include: [
-        {
-          model: Student,
-          as: 'students',
-        },
-      ],
+    .catch((error) => {
+      res.status(400).send(error)
     })
-      .then((holiday) => {
-        if (!holiday) {
-          return res.status(404).send({
-            message: 'Holiday Not Found',
-          })
-        }
-        return holiday
-          .update({
-            class_name: req.body.class_name || holiday.class_name,
-          })
-          .then(() => res.status(200).send(holiday))
-          .catch((error) => res.status(400).send(error))
-      })
-      .catch((error) => res.status(400).send(error))
-  },
-
-  delete(req, res) {
-    return Holiday.findByPk(req.params.id)
-      .then((holiday) => {
-        if (!holiday) {
-          return res.status(400).send({
-            message: 'Holiday Not Found',
-          })
-        }
-        return holiday
-          .destroy()
-          .then(() => res.status(204).send())
-          .catch((error) => res.status(400).send(error))
-      })
-      .catch((error) => res.status(400).send(error))
-  },
+}
+export function getById(req, res) {
+  return Holiday.findByPk(req.params.id, {
+    include: [
+      {
+        model: Student,
+        as: 'students',
+      },
+    ],
+  })
+    .then((holiday) => {
+      if (!holiday) {
+        return res.status(404).send({
+          message: 'Holiday Not Found',
+        })
+      }
+      return res.status(200).send(holiday)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(400).send(error)
+    })
+}
+export function add(req, res) {
+  return Holiday.create({
+    class_name: req.body.class_name,
+  })
+    .then((holiday) => res.status(201).send(holiday))
+    .catch((error) => res.status(400).send(error))
+}
+export function update(req, res) {
+  return Holiday.findByPk(req.params.id, {
+    include: [
+      {
+        model: Student,
+        as: 'students',
+      },
+    ],
+  })
+    .then((holiday) => {
+      if (!holiday) {
+        return res.status(404).send({
+          message: 'Holiday Not Found',
+        })
+      }
+      return holiday
+        .update({
+          class_name: req.body.class_name || holiday.class_name,
+        })
+        .then(() => res.status(200).send(holiday))
+        .catch((error) => res.status(400).send(error))
+    })
+    .catch((error) => res.status(400).send(error))
+}
+export function destroy(req, res) {
+  return Holiday.findByPk(req.params.id)
+    .then((holiday) => {
+      if (!holiday) {
+        return res.status(400).send({
+          message: 'Holiday Not Found',
+        })
+      }
+      return holiday
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch((error) => res.status(400).send(error))
+    })
+    .catch((error) => res.status(400).send(error))
 }
