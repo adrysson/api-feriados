5aa1bbe200534964d76ee0f051af46a69dc4ebf9
Adrysson <adrysson@doity.com.br>
2020-09-01 16:01:47 -0300

    Criando expressões regulares para rota

--- routes/holidays.js
+++ routes/holidays.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const holidayController = require('../controllers').holiday
 
-/* GET users listing. */
-router.get('/:ibge([0-9]{7}|[0-9]{2})', holidayController.index)
+// Expressões regulares
+const regexIbge = '\\d{7}|\\d{2}'
+const regexData = '(\\d{4}\\-)?(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])'
+const regexSlug = '[a-z]+(?:-[a-z]+)*'
 
-module.exports = router;
+/* GET users listing. */
+router.get(
+  `/:ibge(${regexIbge})/:feriado([a-z]+|${regexSlug}|${regexData})`,
+  holidayController.index
+)
+
+module.exports = router
