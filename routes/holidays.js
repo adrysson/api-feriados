1c630deb6e19732fb276d9f5567f6a7901a0e7b5
Adrysson <adrysson@doity.com.br>
2020-09-01 16:03:29 -0300

    Ano obrigatório na regex

--- routes/holidays.js
+++ routes/holidays.js
@@ -4,7 +4,8 @@ const holidayController = require('../controllers').holiday
 
 // Expressões regulares
 const regexIbge = '\\d{7}|\\d{2}'
-const regexData = '(\\d{4}\\-)?(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])'
+const regexData = '\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])'
+const regexDataWithoutYear = '(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])'
 const regexSlug = '[a-z]+(?:-[a-z]+)*'
 
 /* GET users listing. */
